3776059006ad06d7720249341a560721fcab4cb4
VitaliyAndrushko <vitalika1988@gmail.com>
2020-06-15 16:16:05 +0300

    test two

--- git-rename-user.sh
+++ git-rename-user.sh
@@ -2,10 +2,13 @@
 
 # bash script to rename git commit user
 
-git filter-branch --force --env-filter '
-        OLD_EMAIL="vitalika1988@gmail.com"
-        CORRECT_NAME="Vitalika1988"
-        CORRECT_EMAIL="vitalika1988@gmail.com"
+git filter-branch --env-filter '
+        OLD_EMAIL1="vitalika1988@gmail.com"
+        CORRECT_NAME1="VitaliyAndrushko"
+        CORRECT_EMAIL1="vitalika1988@gmail.com"
+        OLD_EMAIL2="front.soft.front@gmail.com"
+        CORRECT_NAME2="Admin"
+        CORRECT_EMAIL2="front.soft.front@gmail.com"
         if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
         then
             export GIT_COMMITTER_NAME="$CORRECT_NAME"
@@ -15,6 +18,16 @@ git filter-branch --force --env-filter '
         then
             export GIT_AUTHOR_NAME="$CORRECT_NAME"
             export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+        fi
+        if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL2" ]
+        then
+            export GIT_AUTHOR_NAME="$CORRECT_NAME2"
+            export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL2"
+        fi
+        if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL2" ]
+        then
+            export GIT_AUTHOR_NAME="$CORRECT_NAME2"
+            export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL2"
         fi
 ' --tag-name-filter cat -- --branches --tags
 
